615eda3a1675a361acfabf7f20c47027767994ee
nicsangom <101397445+nicsangom@users.noreply.github.com>
2023-10-11 18:39:02 +0200

    Add files via upload

---
+++ src/Evento.java
@@ -0,0 +1,57 @@
+public class Evento {
+
+	private Integer id;
+	private String titulo;
+	private String detalles;
+	private String fecha;
+	private String categoria;
+	private String datos;
+	
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDetalles() {
+		return this.detalles;
+	}
+
+	public void setDetalles(String detalles) {
+		this.detalles = detalles;
+	}
+
+	public String getFecha() {
+		return this.fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getCategoria() {
+		return this.categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDatos() {
+		return this.datos;
+	}
+
+	public void setDatos(String datos) {
+		this.datos = datos;
+	}
+}
