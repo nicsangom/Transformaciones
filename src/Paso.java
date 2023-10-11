4b3faf3472f5b4e9c02c1c97ca1108b195a51232
nicsangom <101397445+nicsangom@users.noreply.github.com>
2023-10-11 18:46:35 +0200

    Add files via upload

---
+++ src/Paso.java
@@ -0,0 +1,22 @@
+public class Paso {
+
+	private Integer id;
+	private String nombre;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
