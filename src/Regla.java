999451c10b28ab77ae1e45859e6b3d1ab530c934
nicsangom <101397445+nicsangom@users.noreply.github.com>
2023-10-11 19:53:59 +0200

    Update Regla.java

--- src/Regla.java
+++ src/Regla.java
@@ -1,1 +1,22 @@
+public class Regla {
 
+	private Integer id;
+	private String nombre;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
